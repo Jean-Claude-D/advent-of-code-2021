019b4cd7126098b0befe8eface232c35dd87314f
DesJC <jcd9930@hotmail.ca>
2021-12-02 21:30:03 -0500

    Day 1 : second part

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,10 +1,24 @@
+from typing import List
+
+FS = 3 # frame-size
+skip_value = FS-1
+frame = [0]*FS
 previous = float('inf')
 count = 0
 
+def push(frame: List[int], val: int, fs: int = FS):
+  for i in range(1,fs):
+    frame[-i] = frame[-(i+1)]
+  frame[0] = val
+
 for line in open('input', 'r'):
-  current = int(line)
-  if current > previous:
-    count += 1
-  previous = current
+  push(frame, int(line)) # push new value onto frame
+  current = sum(frame)
+  if skip_value > 0:
+    skip_value -= 1
+  else:
+    if current > previous:
+      count += 1
+    previous = current
 
 print(f'Count is : {count}')
